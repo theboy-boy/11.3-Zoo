2f82a13e56360683fa5f91a4d6cc4ad40d1c3322
theboy_boy - - $0R3N <138262799+theboy-boy@users.noreply.github.com>
2026-02-12 10:35:23 -0600

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -1,45 +1,39 @@
-
 import java.awt.Graphics;
 
-// TODO: add the abstract keyword to the class declaration
-public class Entity {
+public abstract class Entity {
     // optional code to assign a sequencial entity ID
     private static int lastID = 0;
     protected int id;
+    protected boolean alive = true;
+    protected int x;
+    protected int y;
+    protected String name;
+    
 
     // TODO: add the Entity instance variables
 
     public Entity(String name, int x, int y) {
-        // optional code to assign a sequencial entity ID
         this.id = lastID;
-        lastID = lastID + 1;
-
-        // TODO: complete the Entity constructor
+        this.lastID = lastID + 1;
+        this.name = name;
+        this.x = x;
+        this.y = y;
     }
 
-    // ABSTRACT METHODS
-    // tick and draw are called by the Zoo class
-
-    // TODO: add abstract method tick(Zoo z)
-    // TODO: add abstract method draw(Graphics g)
+    public abstract void tick(Zoo z);
+    public abstract void draw(Graphics g);
 
 
-    // NON-ABSTRACT METHODS
-    // isAlive, getX, and getY are all called by the Zoo class
-
-    public boolean isAlive() {
-        // TODO: implement the isAlive method
-        return true;
+    public boolean isAlive(){
+        return alive;
     }
 
     public int getX() {
-        // TODO: implement the getX method
-        return 0;
+        return x;
     }
 
     public int getY() {
-        // implement the getY method
-        return 0;
+        return y;
     }
 
 }
